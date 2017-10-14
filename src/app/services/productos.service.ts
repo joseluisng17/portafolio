c7715c48a8d7d6be42dec6606e7b9ae577bc8c7a
jose luis najar <joseluisng17@gmail.com>
2017-10-14 14:19:22 -0500

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor( private http:Http ) {
@@ -13,9 +14,51 @@ export class ProductosService {
 
   }
 
+  public buscar_producto( termino:string ){
+
+    //console.log("Buscando producto");
+    //console.log( this.productos.length );
+
+    if( this.productos.length === 0 ){
+      this.cargar_productos().then( ( ) => {
+        //termino la carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+       this.filtrar_productos(termino);
+    }
+
+
+
+  }
+
+  private filtrar_productos(termino:string){
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+
+      if( prod.categoria.indexOf( termino ) >=0 || prod.titulo.toLowerCase().indexOf( termino ) >=0 ){
+        this.productos_filtrado.push( prod );
+      }
+
+       //console.log( prod );
+
+    })
+  }
+
+  public cargar_producto( cod:string ){
+    return this.http.get(`https://paginaweb-886f6.firebaseio.com/productos/${ cod }.json`);
+
+  }
+
   public cargar_productos(){
 
     this.cargando = true;
+
+    let promesa = new Promise( ( resolve, reject ) =>{
 
       this.http.get('https://paginaweb-886f6.firebaseio.com/productos_idx.json')
             .subscribe( res => {
@@ -25,8 +68,13 @@ export class ProductosService {
               //setTimeout( ()=>{
               this.cargando = false;
               this.productos = res.json();
+              resolve();
              // },1500)
-            });
+        });
+
+    });
+
+    return promesa;
 
   }
 
